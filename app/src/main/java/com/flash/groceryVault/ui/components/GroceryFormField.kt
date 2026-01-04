44c318f4dcc83aaa41c28278b9a31f047b2f7805
Sridhar Prasath <96571944+sridharprasath94@users.noreply.github.com>
2026-01-04 22:39:26 +0100

    Update suggestion field match mode to prefix

--- app/src/main/java/com/flash/groceryVault/ui/components/GroceryFormField.kt
+++ app/src/main/java/com/flash/groceryVault/ui/components/GroceryFormField.kt
@@ -55,7 +55,7 @@ fun GroceryFormField(
                 onValueChange = { onChange(groceryItems.copy(name = it)) },
                 suggestions = suggestions,
                 label = "Grocery item",
-                matchMode = MatchMode.Contains,
+                matchMode = MatchMode.Prefix,
                 showDropdownIcon = true,
                 modifier = Modifier.fillMaxWidth()
             )
