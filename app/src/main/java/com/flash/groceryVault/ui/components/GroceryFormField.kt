44fc91488376773934d5f304b166302152e5ad92
Sridhar Prasath <96571944+sridharprasath94@users.noreply.github.com>
2026-01-05 22:00:18 +0100

    Optimise the imports

--- app/src/main/java/com/flash/groceryVault/ui/components/GroceryFormField.kt
+++ app/src/main/java/com/flash/groceryVault/ui/components/GroceryFormField.kt
@@ -6,16 +6,11 @@ import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
